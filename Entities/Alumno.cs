034dd2864942c2220fec4fb4ad7660dfadbbdd19
Edwin Tumax <edwintumax@gmail.com>
2020-05-30 12:16:54 -0600

    Iniciando proyecto API

---
+++ Entities/Alumno.cs
@@ -0,0 +1,11 @@
+namespace KalumNotas.Entities
+{
+    public class Alumno
+    {
+        public string Carne {get;set;}
+        public string Apellidos {get;set;}
+        public string Nombres {get;set;}
+        public string NoExpediente {get;set;}
+        public string Email {get;set;}
+    }
+}
---
+++ KalumDBContext/KalumNotasDBContext.cs
@@ -0,0 +1,19 @@
+using KalumNotas.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace KalumNotas.KalumDBContext
+{
+    public class KalumNotasDBContext : DbContext
+    {
+        public KalumNotasDBContext(DbContextOptions<KalumNotasDBContext> options)
+            : base(options)
+        {
+            
+        }        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Alumno>().HasKey( x => new {x.Carne});
+        }
+        public DbSet<Alumno> Alumnos {get;set;}
+    }
+}
